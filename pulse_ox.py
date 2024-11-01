db3cc94cbb655868f71fbf69ef4038629cb3d097
kenbellajaro <kenbellajaro3@gmail.com>
2024-11-01 16:04:42 -0400

    Update pulse_ox.py

--- pulse_ox.py
+++ pulse_ox.py
@@ -15,29 +15,34 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.core.window import Window
-from matplotlib.backends.backend_agg import FigureCanvasAgg
-#from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+from kivy.clock import Clock
+from kivy.graphics import Line, Color
 
 T = 10 # time in seconds that each calc is based on
 color_warning = (254/255,61/255,96/255)
 color_good = (12/255,234/255,194/255,1)
+R_values = []
 # load a subject (1-15 should work)
-daq.load_subject(5)
+daq.load_subject(10)
 
 def back_end(self):
+    #intializing plotting variables
 
     # while there is data left to be read, read it
-    while (daq.curr_index != len(daq.r_data)):
+    if (daq.curr_index != len(daq.r_data)):
+        self.ids.process_data.disabled = True
         r_segment, ir_segment = daq.get_data(T)  # get 10 seconds of data
         # run peak detection on the IR segment
         peak_locs = neurokit_peak_detection.get_peak_locs(ir_segment)[1].get("PPG_Peaks") # the stuff at the end here is just because the thing that is returned has a bunch of info we don't care about right now
         #print(peak_locs)
-        #neurokit_peak_detection.plot_with_peaks(r_segment, ir_segment, peak_locs) # can also use the built in show function from the neurokit module to visualize but that only shows the peaks on the signal it ran the algo on
         HR = (len(peak_locs)-1)/T*60
         print(f"HR: {HR}") # convert the number of peaks detected and the time overwhich they were collected into beats per minute
         ACR, DCR, ACIR, DCIR, R, spo2 = spo2_calculation.calc_spo2(r_segment, ir_segment, peak_locs)
-        self.ids.HR_data.text = f'{HR} bmp'
-        self.ids.spO2_data.text = f'{spo2} %'
+
+        self.ids.HR_data.text = f'{HR} bmp' # updating HR on GUI
+        self.ids.spO2_data.text = f'{spo2} %' # updating spo2 on GUI
+
+        #Indicates on GUI if HR or spo2 are dangerous levels
         if (HR > 100 or HR < 60):
             self.ids.HR_data.color = color_warning
             self.ids.HR_title.color = color_warning
@@ -51,15 +56,38 @@ def back_end(self):
         else:
             self.ids.spO2_data.color = color_good
             self.ids.spO2_title.color = color_good
-
+        
+        #Getting the data points in 10 sec interval
+        plotting_points = [
+            (1000*(x/(daq.f_s*T)), #Getting time stamp of data and scaling it to fit window
+             250*((r_segment[x]-min(r_segment))/(max(r_segment)-min(r_segment))) #Getting pgg data and scaling it to fit window
+             ) 
+             for x in range(len(r_segment))
+            ]
+        
+        #Removing old graph and replacing it with new one
+        self.ids.box.canvas.remove(self.line)
+        self.ids.box.canvas.add(Color(83/255, 214/255, 237/255))
+        self.line = Line(points=plotting_points, width=2)
+        self.ids.box.canvas.add(self.line)
         print(ACR)
         print(DCR)
         print(ACIR)
         print(DCIR)
         print(R)
         print(spo2)
+        R_values.append(R)
         #print("RoR:" + str(R))
-        time.sleep(0.2)
+        #time.sleep(0.2)
+    else: # Once data ends, stops timer and renables button
+        self.ids.process_data.disabled = False
+        self.timer.cancel()
+    R_start = R_values[:2]
+    R_end = R_values[-2:]
+    R_start = sum(R_start)/2
+    R_end = sum(R_end)/2
+    print(f"R_start: {R_start}\nR_end: {R_end}")
+
 
 
 Window.size = (500,700)
@@ -68,14 +96,15 @@ Builder.load_file('pulse_ox.kv')
 
 class PulseOxLayout(Widget):
     def record_data(self):
-        threading.Thread(target=self.update_label).start()
+        self.timer = Clock.schedule_interval(self.update_label, 0.1)
     
-    def update_label(self):
-        self.ids.process_data.disabled = True
-
+    def update_label(self,*args):
         back_end(self)
-
-        self.ids.process_data.disabled = False
+    
+    def __init__(self, **kwargs):
+        super(PulseOxLayout, self).__init__(**kwargs)
+        self.line = Line(points=[], width=2)
+        self.ids.box.canvas.add(self.line)
 
 class PulseOx(App):
     def build(self):
